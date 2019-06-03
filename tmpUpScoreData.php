9b18d1e7cf1e87b4a4e7451a7ff6542e29c37aaf
Shinnshi <35999482+yujianke100@users.noreply.github.com>
2019-06-04 00:03:04 +0800

    Add files via upload

--- tmpUpScoreData.php
+++ tmpUpScoreData.php
@@ -11,10 +11,7 @@ if (!$conn) {
 }
 mysqli_query($conn,'set names utf8');//设置字符集
 $sql = "UPDATE ayaPlayer SET tmpScore = '$score' WHERE name = '$name'";
-$result = mysqli_query($conn, $sql);
-if ($result->num_rows <= 0) {
-    echo "<script>window.location.href='./logIn/SignUp.html';alert('未知账号，排行更新失败！')</script>";
- }
+mysqli_query($conn, $sql);
 
 mysqli_close($conn);
 
